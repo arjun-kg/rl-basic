1eadc57391fecc57ee3ae8b4f665981e2dba12cc
ssllab4 <kg.arjun@gmail.com>
2018-12-11 15:25:07 +0530

    lots of small changes

--- pytorch/ddpg.py
+++ pytorch/ddpg.py
@@ -4,10 +4,14 @@ import torch.nn.functional as F
 import torch.optim as optim 
 import numpy as np 
 from matplotlib import pyplot as plt 
+from collections import namedtuple
 import gym 
+import random
 from copy import deepcopy
 import pdb 
 import time
+
+torch.set_default_tensor_type(torch.DoubleTensor)
 
 #environment name 
 env_name = 'Pendulum-v0'
@@ -15,20 +19,24 @@ env_name = 'Pendulum-v0'
 #random seed
 np.random.seed(1)
 
+#named tuple for experience replay
+Transition = namedtuple('Transition',
+                            ('state', 'action', 'reward','next_state','done'))
+
 #train or test
 train_flag = True
 models_path = './models/ddpg_actor.pkl'
 
 #hyperparam search mode
-hyperparam_search_mode = True
+hyperparam_search_mode = False
 
 #hyperparameters
-max_timesteps = 10000
+max_timesteps = 100000
 num_steps = 1000
 train_batch_size = 64
-noise_factor = 0.01 #gaussian noise added to output of actor network 
-eps_start = 0.5
-T = 100000 #Temperature for epsilon decay
+noise_factor = 0.001 #gaussian noise added to output of actor network 
+eps_start = 0.3
+T = 200000 #Temperature for epsilon decay
 gamma = 0.99 
 tau = 0.001 #target nets are updated by: theta_target <- tau*theta_current + (1-tau)*theta_target
 hidden_layer_size = 256 #all hidden layer sizes in all networks are the same
@@ -39,56 +47,126 @@ class ReplayBuffer:
     def __init__(self,buffer_size=1000000):
         self.buffer_size = buffer_size
         self.buffer = []
+        self.position = 0
     
-    def push(self,transition_tuple):
-        if len(self.buffer) >= self.buffer_size:
-            self.buffer.pop()
-        self.buffer.insert(0,transition_tuple)
+    def push(self,*args):
+
+        transition = Transition(*args)
+        if len(self.buffer) < self.buffer_size:
+            self.buffer.append(None)
+        self.buffer[self.position] = transition
+        self.position = (self.position + 1)%self.buffer_size
     
     def sample(self,n):
-        indices = np.random.choice(len(self.buffer),n)
-        return np.array(self.buffer)[indices]
+        return random.sample(self.buffer, n)
+
+    def __len__(self):
+        return len(self.buffer)
 
 class DDPG_Actor(nn.Module):
     def __init__(self,input_size,output_size):
         super(DDPG_Actor,self).__init__()
         self.lin1 = nn.Linear(input_size,hidden_layer_size)
-        self.lin2 = nn.Linear(hidden_layer_size,output_size)
+        self.lin2 = nn.Linear(hidden_layer_size,hidden_layer_size)
+        self.lin3 = nn.Linear(hidden_layer_size,output_size)
+        self.lin3.weight.data.uniform_(-1e-2,1e-2)
+
+        self.ln1 = nn.LayerNorm(hidden_layer_size)
+        self.ln2 = nn.LayerNorm(hidden_layer_size)
         
     def forward(self,x):
-        x = F.relu(self.lin1(x))
-        x = self.lin2(x)
+        x = F.relu(self.ln1(self.lin1(x)))
+        x = F.relu(self.ln2(self.lin2(x)))
+        x = torch.tanh(self.lin3(x))
         return x
 
 class DDPG_Critic(nn.Module):
-    def __init__(self,input_size, output_size):
+    def __init__(self,state_size,action_size, output_size):
         super(DDPG_Critic,self).__init__()
-        self.lin1 = nn.Linear(input_size,hidden_layer_size)
-        self.lin2 = nn.Linear(hidden_layer_size,output_size)
+        self.lin1 = nn.Linear(state_size,hidden_layer_size)
+        self.lin2 = nn.Linear(hidden_layer_size + action_size,hidden_layer_size)
+        self.lin3 = nn.Linear(hidden_layer_size,output_size)
+        self.lin3.weight.data.uniform_(-1e-2,1e-2)
+
+        self.ln1 = nn.LayerNorm(hidden_layer_size)
+        self.ln2 = nn.LayerNorm(hidden_layer_size)
         
-    def forward(self,x):
-        x = F.relu(self.lin1(x))
-        x = self.lin2(x)
+    def forward(self,state,action):
+        x = F.relu(self.ln1(self.lin1(state)))
+        x = torch.cat((x,action),dim=-1)
+        x = F.relu(self.ln2(self.lin2(x)))
+        x = self.lin3(x)
         return x
+
+class StateNormalizer: 
+    def __init__(self,size):
+        self.mean = np.zeros(size)
+        self.M2 = np.zeros(size)
+        self.count = 0
+        self.std = np.ones(size)
+
+    def normalize(self,x,skip=False):
+        if skip:
+            return x
+        return (x - self.mean)/(self.std+1e-4) #1e-4 is to avoid blow up
+
+    def update(self,x): #uses Welford's online algorithm to calculate variance
+        self.count += 1
+        delta = x - self.mean
+        self.mean += delta/self.count
+        delta2 = x - self.mean
+        self.M2 += delta*delta2
+        self.std = np.sqrt(self.M2/np.maximum((self.count-1),1e-3))
+
+        return (x-self.mean)/self.std
+
+
+class ParameterSpaceNoise:
+    def __init__(self,init_std=0.1, des_action_dev = 0.2, adapt_coeff = 1.01):
+        self.current_std = init_std
+        self.des_action_dev = des_action_dev
+        self.adapt_coeff = adapt_coeff
     
+    def adapt(self,action,noisy_action):
+
+        dist = np.linalg.norm(action-noisy_action,axis=-1) #axis = -1 in case there are batched actions in the future
+
+        if dist < self.des_action_dev:
+            self.current_std *= self.adapt_coeff
+        else:
+            self.current_std /= self.adapt_coeff
+
+    def apply(self,source_policy,noisy_policy):
+        keys = source_policy.state_dict().keys()
+        for key in keys:
+            # pdb.set_trace()   
+            if 'ln' in key: #ignoring the parameters of layer normalization
+                continue
+            par = source_policy.state_dict()[key]
+            n_par = noisy_policy.state_dict()[key]
+
+            new_n_par = par+torch.randn(par.shape) * self.current_std
+            n_par.data.copy_(new_n_par)
+
+
 def train_step(actor_net,critic_net,actor_target_net,critic_target_net,
-                       memory,optimizers,tau):
-    batch = memory.sample(train_batch_size)
-    states = torch.tensor([batch[i][0] for i in range(len(batch))]).float()
-    actions = torch.tensor([batch[i][1] for i in range(len(batch))]).float()
-    rewards = torch.tensor([batch[i][2] for i in range(len(batch))])
-    next_states = torch.tensor([batch[i][3] for i in range(len(batch))]).float()
-    dones = torch.tensor([batch[i][4] for i in range(len(batch))]).float()
+                       memory,optimizers,tau,train_batch_size):
+
+    transitions = memory.sample(train_batch_size)
+    batch = Transition(*zip(*transitions))
+    states = torch.tensor(batch.state)
+    actions = torch.tensor(batch.action)
+    rewards = torch.tensor(batch.reward)
+    next_states = torch.tensor(batch.next_state)
+    dones = torch.tensor(batch.done).double()
 
     #Updating critic net    
     next_state_actions = actor_target_net(next_states)
-    critic_target_net_input = torch.cat([next_states,next_state_actions],dim=-1)
-    critic_target = rewards.view(-1,1) + gamma*critic_target_net(critic_target_net_input)
+    critic_target = rewards.unsqueeze(1) + gamma*(1-dones).unsqueeze(1)*critic_target_net(next_states,next_state_actions)
     
-    critic_net_inputs = torch.cat([states,actions],dim=-1)
-    critic_values = critic_net(critic_net_inputs)
+    critic_values = critic_net(states,actions)
     
-    critic_loss = F.mse_loss(critic_values,critic_target.detach())
+    critic_loss = F.smooth_l1_loss(critic_values,critic_target.detach())
     optimizers[1].zero_grad()
     critic_loss.backward()
     
@@ -99,8 +177,7 @@ def train_step(actor_net,critic_net,actor_target_net,critic_target_net,
 
     #Updating actor net
     actions_from_actor = actor_net(states)
-    critic_net_inputs2 = torch.cat([states,actions_from_actor],dim=-1)
-    actor_loss = - critic_net(critic_net_inputs2).mean()
+    actor_loss = - critic_net(states,actions_from_actor).mean()
     optimizers[0].zero_grad()
     actor_loss.backward()
     
@@ -119,32 +196,47 @@ def train_step(actor_net,critic_net,actor_target_net,critic_target_net,
 
 def add_noise(greedy_action,t,action_max,T,noise_factor,eps_start):
     #Adding Gaussian noise
-    noise = noise_factor*action_max*np.random.rand(greedy_action.shape[-1])
+    # noise = noise_factor*action_max*np.random.rand(greedy_action.shape[-1])
+    noise = 0
     action = greedy_action + noise
-
-    if hyperparam_search_mode:
-        if t > 8000:
-            return action 
-    
+   
     #Epsilon-Greedy
     rnd_num = np.random.rand()
     eps_now = eps_start*np.exp(-t/T)
+
+    if t % 200 == 0:
+        print("Epsilon Now: ",eps_now)
     
     if eps_now < rnd_num:
         return action
     else:
         return np.random.uniform(low=-action_max,high=action_max
                                  ,size=action.shape)
+def evaluate_policy(actor_net,normalizer,env):
+    for _ in range(1):
+        state = env.reset()
+        done = False
+        while not done:
+            env.render()
+            norm_state = normalizer.normalize(state)
+            action = actor_net(torch.tensor(norm_state)).detach().numpy()* env.action_space.high
+            # print(action)
+            print("Mean: ", normalizer.mean,"Std: ",normalizer.std,"state: ", state, "norm_state: ",norm_state, "Action: ",action)
+            next_state,_,done,_ = env.step(action)
+            state = next_state
+
 
 def train_ddpg(train_batch_size,noise_factor, eps_start,T,gamma,tau,hidden_layer_size,lr_actor,lr_critic):
 
     start_time = time.time()
     #environment
     env = gym.make(env_name)
+    action_space_high = env.action_space.high
     
     #initializing class instances
     actor_net = DDPG_Actor(env.observation_space.shape[0],env.action_space.shape[0])
-    critic_net = DDPG_Critic(env.observation_space.shape[0]+env.action_space.shape[0],1)
+    noisy_actor_net = deepcopy(actor_net)
+    critic_net = DDPG_Critic(env.observation_space.shape[0],env.action_space.shape[0],1)
     
     actor_target_net = deepcopy(actor_net)
     actor_target_net.load_state_dict(actor_net.state_dict())
@@ -158,28 +250,48 @@ def train_ddpg(train_batch_size,noise_factor, eps_start,T,gamma,tau,hidden_layer
     optimizers = [optim.Adam(actor_net.parameters(),lr=lr_actor),
                   optim.Adam(critic_net.parameters(),lr=lr_critic)]
     
+    normalizer = StateNormalizer(env.observation_space.shape[0])
+    param_space_noise = ParameterSpaceNoise()
     total_timesteps = 0
+    total_episodes = 0
     rew_list = []
 
     while total_timesteps < max_timesteps:    
         state = env.reset()
         ep_reward = 0
+
+        if total_episodes % 20 == 0:
+            evaluate_policy(actor_net,normalizer,env)
+
         for st in range(num_steps):
-            greedy_action = actor_net(torch.tensor(state).float()).detach().numpy()
-            action = add_noise(greedy_action,total_timesteps,
-                               env.action_space.high,T,noise_factor,eps_start)
             
+            normalizer.update(state)
+            norm_state = normalizer.normalize(state)
+            
+            greedy_action = actor_net(torch.tensor(norm_state)).detach().numpy() * action_space_high
+            noisy_greedy_action = noisy_actor_net(torch.tensor(norm_state)).detach().numpy() * action_space_high
+            param_space_noise.adapt(greedy_action/action_space_high , noisy_greedy_action/action_space_high)
+            param_space_noise.apply(actor_net,noisy_actor_net)
+
+
+            action = add_noise(noisy_greedy_action,total_timesteps,
+                               env.action_space.high,T,noise_factor,eps_start)
+            # print(action)
             next_state,reward,done,_ = env.step(action)
-            transition_tuple = (state,action,reward,next_state,done)
-            memory.push(transition_tuple)
-            if train_flag:
+            next_state = next_state
+            
+            norm_next_state = normalizer.normalize(next_state)
+            memory.push(norm_state,action,reward,norm_next_state,done)
+            if train_flag and len(memory)>= train_batch_size:
                 train_step(actor_net,critic_net,actor_target_net,critic_target_net,
-                           memory,optimizers,tau)
+                           memory,optimizers,tau,train_batch_size)
             total_timesteps += 1
             ep_reward += reward
             # env.render()
             if done or st == num_steps-1:
                 rew_list.append(ep_reward)
+                print("Timesteps: {}, Reward per step: {}, Steps: {}".format(total_timesteps,ep_reward/st,st))
+                total_episodes += 1
                 break
     end_time = time.time()
 
